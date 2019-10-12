797a765cef8aa60269e7bcdaf8f3382ac368ce96
Pablo Cazorla <miscuentas2pablo@gmail.com>
2019-10-12 19:43:44 -0300

    added sass and redux

---
+++ src/redux/reducers.js
@@ -0,0 +1,15 @@
+import { combineReducers } from 'redux';
+import { actName } from './actions';
+
+let reducers = {};
+
+reducers.language = (state = 'es', action) => {
+  switch (action.type) {
+    case actName.SET_LANG:        
+      return action.lang;
+    default:
+      return state;
+  }
+};
+
+export default combineReducers(reducers);
